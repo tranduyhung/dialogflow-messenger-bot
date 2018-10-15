641d13aebcca10fb7f5da031b01ddf7e564b9976
Tran Duy Hung <nguyentieuhau@gmail.com>
2018-10-15 15:46:18 +0700

    Init commit

---
+++ app.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.MONGODB_URI;
+
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+  if (err)
+    throw err;
+
+  console.log(`Connected to ${url}`);
+  db.close();
+  console.log(`Disconnected from ${url}`);
+})
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/api/getProduct', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  if (typeof req.body.name === 'undefined') {
+    res.send(JSON.stringify({ success: false, message: 'Invalid product name' }));
+  }
+
+  res.end();
+});
+
+app.post('/api/getColor', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  if (typeof req.body.name === 'undefined') {
+    res.send(JSON.stringify({ success: false, message: 'Invalid color' }));
+  }
+
+  res.end();
+});
+
+app.post('/api/getSize', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  if (typeof req.body.name === 'undefined') {
+    res.send(JSON.stringify({ success: false, message: 'Invalid size' }));
+  }
+
+  res.end();
+});
+
+app.listen(port, () => console.log(`Listening to port ${port}!`));
---
+++ db.js
@@ -0,0 +1,96 @@
+require('dotenv').config();
+var MongoClient = require('mongodb').MongoClient;
+
+var productsCol = 'products';
+var sizesCol = 'sizes';
+var colorsCol = 'colors';
+var itemsCol = 'items';
+var url = process.env.MONGODB_URI;
+
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+  if (err) throw err;
+
+  var dbo = db.db('heroku_j0v9j0rg');
+
+  dbo.createCollection(productsCol)
+  .then(function() {
+    return dbo.collection(productsCol).drop();
+  })
+  .then(function() {
+    return dbo.createCollection(sizesCol);
+  })
+  .then(function() {
+    return dbo.collection(sizesCol).drop();
+  })
+  .then(function() {
+    return dbo.createCollection(sizesCol);
+  })
+  .then(function() {
+    return dbo.createCollection(colorsCol);
+  })
+  .then(function() {
+    return dbo.collection(colorsCol).drop();
+  })
+  .then(function() {
+    return dbo.createCollection(colorsCol);
+  })
+  .then(function() {
+    var tshirt = { name: 'T-shirt' };
+
+    return dbo.collection(productsCol).insertOne(tshirt);
+  })
+  .then(function() {
+    var jean = { name: 'Jean' };
+
+    return dbo.collection(productsCol).insertOne(jean);
+  })
+  .then(function() {
+    var sizes = [
+      { name: 'S' },
+      { name: 'M' },
+      { name: 'L' },
+    ];
+
+    return dbo.collection(sizesCol).insertMany(sizes);
+  })
+  .then(function() {
+    var colors = [
+      { name: 'Red' },
+      { name: 'Green' },
+      { name: 'Blue' },
+    ];
+
+    return dbo.collection(colorsCol).insertMany(colors);
+  })
+  .then(function() {
+    dbo.collection(productsCol).find({}).toArray(function(err, products) {
+      if (err) throw err;
+
+      for (var i = 0; i < products.length; i++) {
+        var product = products[i];
+
+        dbo.collection(sizesCol).find({}).toArray(function(err, sizes) {
+          if (err) throw err;
+
+          dbo.collection(colorsCol).find({}).toArray(function(err, colors) {
+            if (err) throw err;
+
+            for (var j = 0; j < sizes.length; j++) {
+              var size = sizes[j];
+              for (var k = 0; k < colors.length; k++) {
+                var color = colors[k];
+
+                console.log(product.name + ' ' + size.name + ' ' + color.name);
+              }
+            }
+
+            db.close();
+          })
+        })
+      }
+    })
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+});
