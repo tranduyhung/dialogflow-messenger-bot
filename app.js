4f972f7879d50278c8cd89a1c762768c21cc6a90
Tran Duy Hung <nguyentieuhau@gmail.com>
2018-10-15 16:01:47 +0700

    Used environment port by default

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 80;
+var port = process.env.PORT || 3000;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
